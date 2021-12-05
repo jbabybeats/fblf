271cb74fd26a863768ba3826ad1ee467cee4cb94
Fahmi Dev <52388234+Ainx-BOT@users.noreply.github.com>
2021-12-05 22:10:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!system/bin/sh
 
 pkg update && pkg upgrade -y
+pkg install root-repo -y
+pkg install unstable-repo -y
+pkg install x11-repo -y
 pkg install git python -y
 pkg install curl -y
 termux-setup-storage -y
